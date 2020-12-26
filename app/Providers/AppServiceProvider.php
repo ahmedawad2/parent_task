613c4bea96cb4624d4b7d220df42e2127219b54e
ahmed awad <ahmedawad1811@gmail.com>
2020-12-26 18:51:41 +0200

    made a default implementation for some interfaces

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,15 @@
 
 namespace App\Providers;
 
+use App\Http\Abstraction\Classes\UserTransformerClass;
+use App\Http\Abstraction\Classes\ValidatingUserClass;
+use App\Http\Abstraction\Factories\UsersDTOFactory;
+use App\Http\Abstraction\Interfaces\UsersRepositoryInterface;
+use App\Http\Abstraction\Interfaces\UserTransformInterface;
+use App\Http\Abstraction\Interfaces\ValidatingUserInterface;
+use App\Http\Abstraction\Repositories\UsersRepository;
 use Illuminate\Support\ServiceProvider;
+use pcrov\JsonReader\JsonReader;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +31,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->bind(UsersRepositoryInterface::class, function () {
+            return new UsersRepository(new JsonReader(), request()->get('provider'));
+        });
+        $this->app->bind(UserTransformInterface::class, function () {
+            return new UserTransformerClass();
+        });
+        $this->app->bind(ValidatingUserInterface::class, function (){
+            return new ValidatingUserClass(
+                UsersDTOFactory::create(array_keys(config('data_providers'))[0], [])
+            );
+        });
     }
 }
