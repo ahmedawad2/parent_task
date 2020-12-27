bc7224b9e786c53c901331bf1830c9a70efb4de3
ahmed awad <ahmedawad1811@gmail.com>
2020-12-27 17:53:41 +0200

    moved helpler constants into the config file

--- config/data_providers.php
+++ config/data_providers.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\API\Helpers;
-
 return [
-    Helpers::PROVIDER_X =>\App\Http\Abstraction\Classes\UserXClass::class,
-    Helpers::PROVIDER_Y=>\App\Http\Abstraction\Classes\UserYClass::class,
-//    Helpers::PROVIDER_Z=>\App\Http\Abstraction\Classes\UserZClass::class,
+
+    'providers' => [
+        'DataProviderX' => \App\Http\Abstraction\Classes\UserXClass::class,
+        'DataProviderY' => \App\Http\Abstraction\Classes\UserYClass::class,
+//    'DataProviderZ'=>\App\Http\Abstraction\Classes\UserZClass::class,
+    ],
 ];
